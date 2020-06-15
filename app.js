9a2157101b674a2644b34a654cb5ab725dffcfec
gcampbell391 <gcampbell391@gmail.com>
2020-06-15 15:27:42 -0400

    making progress

--- app.js
+++ app.js
@@ -1,5 +1,58 @@
-const fs = require('fs')
+// const validator = require('validator');
+const yargs = require('yargs')
+const chalk = require('chalk');
+const getNotes = require("./notes.js")
 
-// fs.writeFileSync('notes.txt', "My name is Andrew.")
+//Customize Yargs version
+yargs.version('1.1.0')
 
-fs.appendFileSync('notes.txt', ' I live in Atlanta, Georgia!')
+//Create add command
+yargs.command({
+    command: "add",
+    describe: "add a new note",
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ', argv.title)
+        console.log('Body: ', argv.body)
+    }
+})
+//Create remove command
+yargs.command({
+    command: "remove",
+    describe: "remove a note",
+    handler: function () {
+        console.log("Removing the note!")
+    }
+})
+//Create the list command
+yargs.command({
+    command: "list",
+    describe: "list your notes",
+    handler: function () {
+        console.log("listing out all notes!")
+    }
+})
+//Create the read command
+yargs.command({
+    command: "read",
+    describe: "read a note",
+    handler: function () {
+        console.log("reading a note!")
+    }
+})
+//add, remove, read, list notes
+
+yargs.parse()
+
+
